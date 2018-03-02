04de24f7bb73882bf6fc08fdbeb6ed9e4a42a7bb
omareN11 <36748398+omareN11@users.noreply.github.com>
2018-03-01 21:26:39 -0600

    ClienteAgente
    Agrege las pantallas de Cliente y Agente pero aun no las comento

---
+++ ProyectoSQLServer/FormAgent.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoSQLServer
+{
+    public partial class FormAgent : Form
+    {
+        private DatabaseConnection connection;
+        private int idAgente = -1;
+        private string tableName = "Agente";
+
+        public FormAgent()
+        {
+            InitializeComponent();
+            connection = new DatabaseConnection(@"DESKTOP-91T9VRR\SQLEXPRESS", "databasecar");
+            dataGridViewAgent.DataSource = connection.Refresh(tableName, "*");
+            propertyFormAgent();
+        }
+
+        private void propertyFormAgent()
+        {
+            textBoxNameAgent.MaxLength = textBoxAddress.MaxLength = textBoxEmail.MaxLength = 25;
+            textBoxRFCAgent.MaxLength = 12;
+            textBoxPhone.MaxLength = 15;
+            numericUpSalary.Maximum = int.MaxValue;
+        }
+
+        private bool existEmptyFields()
+        {
+            bool res = false;
+            foreach (Control control in this.Controls)
+            {
+                if (control.GetType() == typeof(TextBox))
+                {
+                    if (control.Text.Length == 0)
+                    {
+                        res = true;
+                        break;
+                    }
+                }
+            }
+            return (res);
+        }
+
+        private string generateStringValue(string action)
+        {
+            string aux = "";
+            switch (action)
+            {
+                case "INSERT":
+                    aux = "'" + textBoxNameAgent.Text + "','" + textBoxRFCAgent.Text + "','" + numericUpSalary.Value.ToString() + "','" + textBoxAddress.Text + "','" + textBoxPhone.Text + "','" + textBoxEmail.Text + "'";
+                    break;
+                case "UPDATE":
+                    aux = "Nombre='" + textBoxNameAgent.Text + "',RFC='" + textBoxRFCAgent.Text + "',Salario='" + numericUpSalary.Value.ToString() + "',Domicilio='" + textBoxAddress.Text + "',Celular='" + textBoxPhone.Text + "',Email='" + textBoxEmail.Text + "'";
+                    break;
+            }
+
+            return (aux);
+        }
+
+        private void cleanForm()
+        {
+            foreach (Control control in this.Controls)
+            {
+                if (control.GetType() == typeof(TextBox))
+                    control.Text = "";
+                else
+                     if (control.GetType() == typeof(NumericUpDown))
+                    numericUpSalary.Value = 0;
+            }
+            idAgente = -1;
+        }
+
+        private void buttonInsert_Click(object sender, EventArgs e)
+        {
+            if (existEmptyFields() != true)
+            {
+                connection.InsertInto(tableName, "Nombre,RFC,Salario,Domicilio,Celular,Email", generateStringValue("INSERT"));
+                dataGridViewAgent.DataSource = connection.Refresh(tableName, "*");
+                cleanForm();
+            }
+            else
+                MessageBox.Show("Es Obligatorio llenar todos los campos");
+        }
+
+        private void buttonModify_Click(object sender, EventArgs e)
+        {
+            if (idAgente != -1)
+            {
+                if (existEmptyFields() != true)
+                {
+                    connection.UpdateSet(tableName, idAgente.ToString(), generateStringValue("UPDATE"));
+                    dataGridViewAgent.DataSource = connection.Refresh(tableName, "*");
+                    cleanForm();
+                }
+                else
+                    MessageBox.Show("Es OBLIGATORIO llenar todos los campos");
+            }
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            if (idAgente != -1)
+            {
+                connection.DeleteFrom(tableName, idAgente.ToString());
+                dataGridViewAgent.DataSource = connection.Refresh(tableName, "*");
+                cleanForm();
+            }
+        }
+
+        private void dataGridViewAgent_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            textBoxNameAgent.Text = dataGridViewAgent[1, e.RowIndex].Value.ToString();
+            textBoxRFCAgent.Text = dataGridViewAgent[2, e.RowIndex].Value.ToString();
+            numericUpSalary.Value = (decimal)Convert.ToSingle(dataGridViewAgent[3, e.RowIndex].Value.ToString());
+            textBoxAddress.Text = dataGridViewAgent[4, e.RowIndex].Value.ToString();
+            textBoxPhone.Text = dataGridViewAgent[5, e.RowIndex].Value.ToString();
+            textBoxEmail.Text = dataGridViewAgent[6, e.RowIndex].Value.ToString();
+            idAgente = Convert.ToInt32(dataGridViewAgent[0, e.RowIndex].Value.ToString());
+        }
+    }
+}
---
+++ ProyectoSQLServer/FormClient.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace ProyectoSQLServer
+{
+    partial class FormClient
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonModify = new System.Windows.Forms.Button();
+            this.buttonInsert = new System.Windows.Forms.Button();
+            this.textBoxOcupation = new System.Windows.Forms.TextBox();
+            this.textBoxPhone = new System.Windows.Forms.TextBox();
+            this.textBoxAddress = new System.Windows.Forms.TextBox();
+            this.textBoxNameClient = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridViewClient = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewClient)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(292, 118);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(158, 23);
+            this.buttonDelete.TabIndex = 56;
+            this.buttonDelete.Text = "ELIMINAR";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // buttonModify
+            // 
+            this.buttonModify.Location = new System.Drawing.Point(292, 76);
+            this.buttonModify.Name = "buttonModify";
+            this.buttonModify.Size = new System.Drawing.Size(158, 23);
+            this.buttonModify.TabIndex = 55;
+            this.buttonModify.Text = "MODIFICAR";
+            this.buttonModify.UseVisualStyleBackColor = true;
+            this.buttonModify.Click += new System.EventHandler(this.buttonModify_Click);
+            // 
+            // buttonInsert
+            // 
+            this.buttonInsert.Location = new System.Drawing.Point(292, 32);
+            this.buttonInsert.Name = "buttonInsert";
+            this.buttonInsert.Size = new System.Drawing.Size(158, 23);
+            this.buttonInsert.TabIndex = 54;
+            this.buttonInsert.Text = "INSERTAR";
+            this.buttonInsert.UseVisualStyleBackColor = true;
+            this.buttonInsert.Click += new System.EventHandler(this.buttonInsert_Click);
+            // 
+            // textBoxOcupation
+            // 
+            this.textBoxOcupation.Location = new System.Drawing.Point(85, 139);
+            this.textBoxOcupation.Name = "textBoxOcupation";
+            this.textBoxOcupation.Size = new System.Drawing.Size(181, 20);
+            this.textBoxOcupation.TabIndex = 53;
+            // 
+            // textBoxPhone
+            // 
+            this.textBoxPhone.Location = new System.Drawing.Point(85, 98);
+            this.textBoxPhone.Name = "textBoxPhone";
+            this.textBoxPhone.Size = new System.Drawing.Size(181, 20);
+            this.textBoxPhone.TabIndex = 52;
+            // 
+            // textBoxAddress
+            // 
+            this.textBoxAddress.Location = new System.Drawing.Point(85, 57);
+            this.textBoxAddress.Name = "textBoxAddress";
+            this.textBoxAddress.Size = new System.Drawing.Size(181, 20);
+            this.textBoxAddress.TabIndex = 51;
+            // 
+            // textBoxNameClient
+            // 
+            this.textBoxNameClient.Location = new System.Drawing.Point(85, 16);
+            this.textBoxNameClient.Name = "textBoxNameClient";
+            this.textBoxNameClient.Size = new System.Drawing.Size(181, 20);
+            this.textBoxNameClient.TabIndex = 50;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(11, 142);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(70, 13);
+            this.label6.TabIndex = 49;
+            this.label6.Text = "OCUPACIÓN";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(11, 101);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 13);
+            this.label5.TabIndex = 48;
+            this.label5.Text = "TELEFONO\r\n";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(8, 60);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 13);
+            this.label4.TabIndex = 47;
+            this.label4.Text = "DOMICILIO";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 46;
+            this.label1.Text = "NOMBRE";
+            // 
+            // dataGridViewClient
+            // 
+            this.dataGridViewClient.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewClient.Location = new System.Drawing.Point(17, 179);
+            this.dataGridViewClient.Name = "dataGridViewClient";
+            this.dataGridViewClient.Size = new System.Drawing.Size(436, 166);
+            this.dataGridViewClient.TabIndex = 45;
+            this.dataGridViewClient.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewClient_CellClick);
+            // 
+            // FormClient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(460, 360);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonModify);
+            this.Controls.Add(this.buttonInsert);
+            this.Controls.Add(this.textBoxOcupation);
+            this.Controls.Add(this.textBoxPhone);
+            this.Controls.Add(this.textBoxAddress);
+            this.Controls.Add(this.textBoxNameClient);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridViewClient);
+            this.Name = "FormClient";
+            this.Text = "FormClient";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewClient)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonModify;
+        private System.Windows.Forms.Button buttonInsert;
+        private System.Windows.Forms.TextBox textBoxOcupation;
+        private System.Windows.Forms.TextBox textBoxPhone;
+        private System.Windows.Forms.TextBox textBoxAddress;
+        private System.Windows.Forms.TextBox textBoxNameClient;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridViewClient;
+    }
+}
